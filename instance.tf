b70ca32d628593fb508b45541ce49adda61f25b6
andrey.deputatov <effko32@yandex.ru>
2022-12-11 19:29:41 +0400

    Fix instance.tf

--- instance.tf
+++ instance.tf
@@ -2,14 +2,14 @@ resource "vkcs_compute_instance" "basic" {
   name            = "basic"
   image_id        = data.vkcs_images_image.ubuntu.id
   flavor_id       = data.vkcs_compute_flavor.small.id
-  key_pair        = "keypair_1"
-  security_groups = ["secgroup_1"]
+  key_pair        = vkcs_compute_keypair.keypair_1.name
+  security_groups = [vkcs_networking_secgroup.secgroup_1.name]
 
   metadata = {
-    this = "that"
+    terraform = "true"
   }
 
   network {
-    name = "network_1"
+    uuid = vkcs_networking_network.network_1.id
   }
 }
