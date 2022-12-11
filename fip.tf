5f9b81bf90f07f42fba2be7e0dce811d80a0553f
andrey.deputatov <effko32@yandex.ru>
2022-12-11 19:40:13 +0400

    Add floating ip

---
+++ fip.tf
@@ -0,0 +1,8 @@
+resource "vkcs_networking_floatingip" "fip" {
+  pool = data.vkcs_networking_network.extnet.name
+}
+
+resource "vkcs_compute_floatingip_associate" "fip" {
+  floating_ip = vkcs_networking_floatingip.fip.address
+  instance_id = vkcs_compute_instance.basic.id
+}
