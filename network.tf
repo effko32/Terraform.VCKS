123195d337b48cd7bd3829eaf7b7b3aedd189de1
andrey.deputatov <effko32@yandex.ru>
2022-12-11 19:39:48 +0400

    Fix network.tf

--- network.tf
+++ network.tf
@@ -1,10 +1,25 @@
-resource "vkcs_networking_network" "network_1" {
+data "vkcs_networking_network" "extnet" {
+  name = "ext-net"
+}
+
+resource "vkcs_networking_network" "compute" {
   name           = "network_1"
   admin_state_up = "true"
 }
 
-resource "vkcs_networking_subnet" "subnet_1" {
+resource "vkcs_networking_subnet" "compute" {
   name       = "subnet_1"
-  network_id = "${vkcs_networking_network.network_1.id}"
+  network_id = "${vkcs_networking_network.compute.id}"
   cidr       = "192.168.199.0/24"
+}
+
+resource "vkcs_networking_router" "compute" {
+  name                = "router_1"
+  admin_state_up      = true
+  external_network_id = data.vkcs_networking_network.extnet.id
+}
+
+resource "vkcs_networking_router_interface" "compute" {
+  router_id = vkcs_networking_router.compute.id
+  subnet_id = vkcs_networking_subnet.compute.id
 }
