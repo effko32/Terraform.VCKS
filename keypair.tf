4c1ee488f9810b805ee04c737a59d6a8c8841110
andrey.deputatov <effko32@yandex.ru>
2023-01-13 16:21:57 +0400

    Fix keypair

--- keypair.tf
+++ keypair.tf
@@ -1,7 +1,12 @@
 resource "vkcs_compute_keypair" "keypair_1" {
   name = "keypair_1"
 
-  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
-    command = "echo '${vkcs_compute_keypair.keypair_1.private_key}' > ./keypair_1_private_key.pem"
-  }
+  # provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+  #   command = "echo '${vkcs_compute_keypair.keypair_1.private_key}' > ./keypair_1_private_key.pem"
+  # }
+}
+
+resource "local_file" "keypair_ssh" {
+  content  = "${vkcs_compute_keypair.keypair_1.private_key}"
+  filename = "./hosts/keypair_ssh.pem"
 }
